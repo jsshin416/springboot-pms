11431c0845b5499f3cbbbb8f1bef663f1f866425
jsshin416 <49501965+jsshin416@users.noreply.github.com>
2021-05-15 17:54:30 +0900

    Delete src/main/java/edu/axboot/domain/pms/room directory

--- src/main/java/edu/axboot/domain/pms/room/PmsRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.axboot.domain.pms.room;
-
-import com.chequer.axboot.core.domain.base.AXBootJPAQueryDSLRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PmsRoomRepository extends JpaRepository<PmsRoom, Long> {
-}
